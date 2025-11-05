310ae1f1aa23ab85b2d184a56d9142f30325b80b
Luim3141 <hiepk40c@gmail.com>
2025-11-05 10:56:28 +0700

    edit readme and and lib and database folder

--- README.md
+++ README.md
@@ -15,8 +15,21 @@ A Java Swing-based library management system built with **NetBeans / IntelliJ ID
 
 ---
 
-## 🧩 Database Setup
+## 🧩 Database Setup(optional)
 
 1. Create a MySQL database named `library`.
 2. Import the required tables manually from your project (e.g., `book`, `student`, `login`, `request`, etc.).
 3. Update your database connection details inside:
+
+
+## ⚙️ Setup Instructions
+1. Import `database/library.sql` into MySQL.
+2. Open project in IntelliJ or NetBeans.
+3. Add `lib/mysql-connector-j-8.3.0.jar` to project libraries.
+4. Run `SignIn.java`
+
+## 📂 Project Structure
+- `src/system`: All source code (UI + Logic)
+- `img/`: Application icons and background
+- `lib/`: MySQL JDBC Driver
+- `database/`: SQL dump of the database
---
+++ database/library.sql
@@ -0,0 +1,142 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `id` varchar(15) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL,
+  `publisher` varchar(45) DEFAULT NULL,
+  `price` varchar(30) DEFAULT NULL,
+  `year` varchar(30) DEFAULT NULL,
+  `status` varchar(35) DEFAULT NULL,
+  `issuedate` varchar(60) DEFAULT NULL,
+  `duedate` varchar(60) DEFAULT NULL,
+  `studentid` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES ('A1','Genade','Re','Ear','10000','2009','Issued','02/11/2025','04/11/2025',1),('B1','Reverend','Zhi','Gu','1000','2020','Issued','02/11/2025','04/2/2026',2),('C3','haha','Hehe','Hoho','11','2000','Issued','02/11/2025','',3),('D4','shds','sdfs','323','232','231','NotIssued',NULL,NULL,NULL),('E5','hehi','test','tut','123','2004','Issued',NULL,NULL,4);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `login` (
+  `userid` varchar(35) NOT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `role` varchar(20) DEFAULT 'client',
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES ('','','client'),('admin','123','admin'),('haha','123','client'),('hiepga','123','admin'),('luim','123','admin'),('thuyngoc','123','client');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `request`
+--
+
+DROP TABLE IF EXISTS `request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `request` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `book_id` varchar(20) DEFAULT NULL,
+  `book_name` varchar(255) DEFAULT NULL,
+  `student_id` varchar(20) DEFAULT NULL,
+  `student_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `request`
+--
+
+LOCK TABLES `request` WRITE;
+/*!40000 ALTER TABLE `request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `id` int NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `course` varchar(30) DEFAULT NULL,
+  `branch` varchar(15) DEFAULT NULL,
+  `semester` varchar(10) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT 'Male',
+  `class` varchar(50) DEFAULT 'IT01',
+  `email` varchar(100) DEFAULT 'student@example.com',
+  `phone` varchar(15) DEFAULT '0901234567',
+  `address` varchar(200) DEFAULT 'Hanoi, Vietnam',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'Hiep','Math','Edu','2023','Male','IT01','Hiephaha','0131','NA'),(2,'Dai','Math','IT','2024','Male','IT01','student@example.com','0901234567','Hanoi, Vietnam'),(3,'Duy','Math','IT','2022','Male','ITe10','duyhaha@','0110','NA'),(4,'ds','23','df','2342','Male','2r','24','234','aaaa');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-05 10:37:29
